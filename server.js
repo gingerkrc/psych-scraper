70136e2db75c7f8000315702f62a4eddbd8f885a
Ginger KRC <gingerkr@gmail.com>
2019-02-17 16:48:21 -0700

    another heroku attempt

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var cheerio = require("cheerio");
 var db = require("./models");
 var path = require("path");
 
-var PORT = 3000 || process.env.PORT;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
